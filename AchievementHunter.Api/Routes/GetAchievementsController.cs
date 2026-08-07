9c6540eb8341e4f400e67ecb9ca213b5610c1fa5
DarylvdBerg <darylvdberg@gmail.com>
2026-08-07 14:35:43 +0200

    feat: setup get achievements controller

---
+++ AchievementHunter.Api/Routes/GetAchievementsController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Steam.Api.Routes;
+
+[ApiController]
+[ApiVersion("1.0")]
+[Route("/api/v{version:apiVersion}/[controller]")]
+public class GetAchievementsController : ControllerBase
+{
+    [HttpGet, MapToApiVersion("1.0")]
+    public ActionResult<String> Get()
+    {
+        return Ok("GetAchievements");
+    }
+}
